9233e989feba402e87dc2392392dd9aff54567d6
Ace Goibniu <4308322+acegoibniu@users.noreply.github.com>
2020-01-01 16:52:00 -0500

    Added scripts for generating the TXT report.
    Added script function for generating the content for the TXT report.

--- Scripts.js
+++ Scripts.js
@@ -40,3 +40,39 @@ function ProduceMDTableByGameMode(MapsData) {
     
   return (Result);
 }
+
+function ProduceTXTReport(MapsData, GameMode) {
+  'use strict';
+  var Result = "";
+  MapsData.forEach( (M) => {
+    if (!GameMode || M.mode.toLowerCase() == GameMode.toLowerCase()) {
+      Result += `${M.name.padEnd(42, ' ')}${M.creator.padEnd(60, ' ')}${M.comment}\n`;
+    }
+  });
+  return (Result);
+}
+
+function ProduceTXTReportByGameMode(MapsData) {
+  'use strict';
+  var Result =
+      `${"Map Name:".padEnd(42, ' ')}${"Author(s):".padEnd(60, ' ')}Other Info:\n"`;
+  Result +=
+    `${"====================================".padEnd(42, ' ')}${"-----------------------------------------".padEnd(60, ' ')}**************************************************\n"`;
+  
+  Result += "===Deathmatch/Team DM/Hunt/Survivor Levels===\n\n";
+  Result += ProduceTXTReport(MapsData, "DM");
+    
+  Result += "\n\n===Team Deathmatch Only Levels===\n\n";
+  Result += ProduceTXTReport(MapsData, "TEAMDM");
+  
+  Result += "\n\n===Survivor Only Levels===\n\n";
+  Result += ProduceTXTReport(MapsData, "SURVIVOR");
+  
+  Result += "\n\n===Overrun Levels===\n\n";
+  Result += ProduceTXTReport(MapsData, "OVERRUN");
+  
+  Result += "\n\n===Evac Levels===\n\n";
+  Result += ProduceTXTReport(MapsData, "EVAC");
+    
+  return (Result);
+}
