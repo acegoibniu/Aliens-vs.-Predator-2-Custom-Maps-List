3a1122f05884ad38fbb10100ab26401cf1a0014f
Ace Goibniu <4308322+acegoibniu@users.noreply.github.com>
2020-01-01 13:12:15 -0500

    Fixed formatting issues in Scripts.js
    Multiple lines had undesirable indentation.

--- Scripts.js
+++ Scripts.js
@@ -9,34 +9,34 @@ function ProduceCSV(MapsData) {
 
 
 function ProduceMDTable(MapsData, GameMode) {
-    'use strict';
-    var Result = "|Map Name|Author(s)|Comment|\n";
-    Result += 
+  'use strict';
+  var Result = "|Map Name|Author(s)|Comment|\n";
+  Result += 
     "|---------------------------------------|---------------------------------------|---------------------------------------|\n";
-    MapsData.forEach( (M) => {
+  MapsData.forEach( (M) => {
     if (!GameMode || M.mode.toLowerCase() == GameMode.toLowerCase()) {
-    Result += `|${M.name}|${M.creator}|${M.comment}|\n`;
-  }
+      Result += `|${M.name}|${M.creator}|${M.comment}|\n`;
+    }
   });
-    return (Result);
+  return (Result);
 }    
     
 function ProduceMDTableByGameMode(MapsData) {
-    'use strict';
-    var Result = "## Deathmatch/Team DM/Hunt/Survivor Levels\n\n";
-    Result += ProduceMDTable(MapsData, "DM");
+  'use strict';
+  var Result = "## Deathmatch/Team DM/Hunt/Survivor Levels\n\n";
+  Result += ProduceMDTable(MapsData, "DM");
     
-    Result += "\n\n## Team Deathmatch Only Levels\n\n";
-    Result += ProduceMDTable(MapsData, "TEAMDM");
+  Result += "\n\n## Team Deathmatch Only Levels\n\n";
+  Result += ProduceMDTable(MapsData, "TEAMDM");
+  
+  Result += "\n\n## Survivor Only Levels\n\n";
+  Result += ProduceMDTable(MapsData, "SURVIVOR");
+  
+  Result += "\n\n## Overrun Levels\n\n";
+  Result += ProduceMDTable(MapsData, "OVERRUN");
+  
+  Result += "\n\n## Evac Levels\n\n";
+  Result += ProduceMDTable(MapsData, "EVAC");
     
-    Result += "\n\n## Survivor Only Levels\n\n";
-    Result += ProduceMDTable(MapsData, "SURVIVOR");
-    
-    Result += "\n\n## Overrun Levels\n\n";
-    Result += ProduceMDTable(MapsData, "OVERRUN");
-    
-    Result += "\n\n## Evac Levels\n\n";
-    Result += ProduceMDTable(MapsData, "EVAC");
-    
-    return (Result);
+  return (Result);
 }
